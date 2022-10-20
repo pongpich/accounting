5eae9170774f409b57209d89169f1d12ea3680d5
Tanakorn Numrubporn <him_aeng@yahoo.co.th>
2022-10-20 10:27:31 +0700

    First commit

---
+++ js/util.js
@@ -0,0 +1,24 @@
+function getURLParams(name) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+
+  return urlParams.get(name);
+}
+
+// Pass the checkbox name to the function
+function getCheckedBoxes(chkboxName) {
+  var checkboxes = document.getElementsByName(chkboxName);
+  var checkboxesChecked = [];
+  // loop over them all
+  for (var i=0; i<checkboxes.length; i++) {
+     // And stick the checked ones onto an array...
+     if (checkboxes[i].checked) {
+        checkboxesChecked.push(checkboxes[i].value);
+     }
+  }
+  // Return the array if it is non-empty, or null
+  return checkboxesChecked.length > 0 ? checkboxesChecked : null;
+}
+
+//รหัสโปรแกรมสำหรับเลือกว่าจะสมัครรุ่นไหน โปรแกรมอะไร
+const serviceUrl = 'https://api.ccrdiet.co/coredev';
